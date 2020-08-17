74dfa57a976804603a0bf481b3cbd749cc61045c
farbodkain <farbodkain@gmail.com>
2020-08-17 23:45:05 +0430

    implement attack method

--- js/app.js
+++ js/app.js
@@ -11,9 +11,41 @@ new Vue({
       this.monsterHealth = 100;
       this.playerHealth = 100;
     },
-    attack: function () {},
-    specialAttack: function () {},
-    heal: function () {},
-    giveUp: function () {},
+    attack: function () {
+
+      this.monsterHealth -= this.calculateDamage(10, 3);
+
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.playerHealth -= this.calculateDamage(12, 5);
+      
+      this.checkWin();
+    },
+    specialAttack: function () { },
+    heal: function () { },
+    giveUp: function () { },
+    calculateDamage: function (max, min) {
+      return Math.max((Math.floor((Math.random() * max)) + 1), min);
+    },
+    checkWin: function () {
+      if (this.monsterHealth <= 0) {
+        if (confirm("You won,Start new Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm("You lost,Start new Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    }
   },
 });
