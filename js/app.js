03365533ad5f08dcaf6dd17d28ec1288d0e86f0c
Farbod Rostami <f.rostami@ebidar.com>
2020-08-18 09:12:48 +0430

    Monster Slayer Developement complete

--- js/app.js
+++ js/app.js
@@ -4,30 +4,56 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false,
+    turns: [],
   },
   methods: {
     startGame: function () {
       this.gameIsRunning = true;
       this.monsterHealth = 100;
       this.playerHealth = 100;
+      this.turns = [];
     },
     attack: function () {
-
-      this.monsterHealth -= this.calculateDamage(10, 3);
-
+      var damage = this.calculateDamage(10, 3);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Playar hit monster for " + damage,
+      });
       if (this.checkWin()) {
         return;
       }
-
-      this.playerHealth -= this.calculateDamage(12, 5);
-      
-      this.checkWin();
+      this.monsterAttack();
     },
-    specialAttack: function () { },
-    heal: function () { },
-    giveUp: function () { },
+    specialAttack: function () {
+      var damage = this.calculateDamage(20, 8);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Playar special hit monster for " + damage,
+      });
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttack();
+    },
+    heal: function () {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player heal by 10",
+      });
+      this.monsterAttack();
+    },
+    giveUp: function () {
+      this.gameIsRunning = false;
+    },
     calculateDamage: function (max, min) {
-      return Math.max((Math.floor((Math.random() * max)) + 1), min);
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
     checkWin: function () {
       if (this.monsterHealth <= 0) {
@@ -46,6 +72,15 @@ new Vue({
         return true;
       }
       return false;
-    }
+    },
+    monsterAttack: function () {
+      var damage = this.calculateDamage(12, 5);
+      this.playerHealth -= damage;
+      this.turns.unshift({
+        isPlayer: false,
+        text: "Monster hit player for " + damage,
+      });
+      this.checkWin();
+    },
   },
 });
